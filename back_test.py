2c1f3dc1b4224731cbcd64427e15b7c202af0a3a
mrcrysmile <275076698@qq.com>
2020-03-04 18:18:06 +0800

    98

--- back_test.py
+++ back_test.py
@@ -9,6 +9,7 @@ import random
 
 
 #TODO: nnn在年关附近测试
+# 7 的过拟合测试
 
 # 当前日期
 today = int(time.strftime("%Y%m%d", time.localtime()))
@@ -18,8 +19,8 @@ class HoldStock():
     # 计算卖出日期与利率
     def cal_sell(self):
         hold_max_day = 5
-        sell_max_profit = 1.16
-        sell_min_profit = 0.91
+        sell_max_profit = 1.15
+        sell_min_profit = 0.93
         if self.data.shape[0] >= hold_max_day:
             for i in range(0, hold_max_day):
                 self.p *= (self.data.iloc[i]["ChangeRatio"] + 1)
@@ -75,24 +76,24 @@ def back_test(data, test_years, max_stockhold, tradingdate):
     total = {"5_10": 1.0, "nnn":1.0, "7":1.0}
     balance = {"5_10": 1.0, "nnn":1.0, "7":1.0}
     # 记录持股
-    stock_hold_5_10 = []
+    # stock_hold_5_10 = []
     # stock_hold_7 = []
-    # stock_hold_nnn = []
+    stock_hold_nnn = []
     # 记录每次卖出收益
     sell = {"5_10": [], "7": [], "nnn": []}
     # 回测开始
     for day in cal_date:
         # 持股不满时才需要计算
-        if len(stock_hold_5_10) < 5:
-            # 获取筛选结果
-            sel_5_10 = cal_5_10(data, day, tradingdate, 0.25, 0.16)
-            for i in range(0, 5 - len(stock_hold_5_10)):
-                if len(sel_5_10) > i:
-                    stock_data = data[data['Symbol'] == sel_5_10[i]]
-                    stock_data = stock_data[stock_data['TradingDate'] > day]
-                    spend = min(balance["5_10"], total["5_10"] / 5)
-                    balance["5_10"] -= spend
-                    stock_hold_5_10.append(HoldStock(sel_5_10[i], spend, stock_data))
+        # if len(stock_hold_5_10) < 5:
+        #     # 获取筛选结果
+        #     sel_5_10 = cal_5_10(data, day, tradingdate, -0.1, -0.16)
+        #     for i in range(0, 5 - len(stock_hold_5_10)):
+        #         if len(sel_5_10) > i:
+        #             stock_data = data[data['Symbol'] == sel_5_10[i]]
+        #             stock_data = stock_data[stock_data['TradingDate'] > day]
+        #             spend = min(balance["5_10"], total["5_10"] / 5)
+        #             balance["5_10"] -= spend
+        #             stock_hold_5_10.append(HoldStock(sel_5_10[i], spend, stock_data))
         # if len(stock_hold_7) < 5:
         #     # 获取筛选结果
         #     sel_7 = cal_7(data, day)
@@ -102,25 +103,25 @@ def back_test(data, test_years, max_stockhold, tradingdate):
         #         spend = min(balance["7"], total["7"] / 5)
         #         balance["7"] -= spend
         #         stock_hold_7.append(HoldStock(sel_7[i], spend, stock_data))
-        # if len(stock_hold_nnn) < 5:
-        #     # 获取筛选结果
-        #     sel_nnn = cal_nnn(data, day, tradingdate)
-        #     for i in range(0, 5 - len(stock_hold_nnn)):
-        #         if len(sel_nnn) > i:
-        #             stock_data = data[data['Symbol'] == sel_nnn[i][0]]
-        #             stock_data = stock_data[stock_data['TradingDate'] > day]
-        #             spend = min(balance["nnn"], total["nnn"] / 5)
-        #             balance["nnn"] -= spend
-        #             stock_hold_nnn.append(HoldStock(sel_nnn[i], spend, stock_data))
+        if len(stock_hold_nnn) < 5:
+            # 获取筛选结果
+            sel_nnn = cal_nnn(data, day, tradingdate)
+            for i in range(0, 5 - len(stock_hold_nnn)):
+                if len(sel_nnn) > i:
+                    stock_data = data[data['Symbol'] == sel_nnn[i][0]]
+                    stock_data = stock_data[stock_data['TradingDate'] > day]
+                    spend = min(balance["nnn"], total["nnn"] / 5)
+                    balance["nnn"] -= spend
+                    stock_hold_nnn.append(HoldStock(sel_nnn[i], spend, stock_data))
             
-        # 计算卖出
-        for hold in stock_hold_5_10:
-            if int(day) == int(hold.enddate):
-                total["5_10"] += hold.amount * (hold.p - 1)
-                balance["5_10"] += hold.amount * hold.p
-                sell["5_10"].append(hold.p)
-                stock_hold_5_10.remove(hold)
-                del hold
+        # # 计算卖出
+        # for hold in stock_hold_5_10:
+        #     if int(day) == int(hold.enddate):
+        #         total["5_10"] += hold.amount * (hold.p - 1)
+        #         balance["5_10"] += hold.amount * hold.p
+        #         sell["5_10"].append(hold.p)
+        #         stock_hold_5_10.remove(hold)
+        #         del hold
 
         # for hold in stock_hold_7:
         #     if int(day) == int(hold.enddate):
@@ -130,13 +131,13 @@ def back_test(data, test_years, max_stockhold, tradingdate):
         #         stock_hold_7.remove(hold)
         #         del hold
 
-        # for hold in stock_hold_nnn:
-        #     if int(day) == int(hold.enddate):
-        #         total["nnn"] += hold.amount * (hold.p - 1)
-        #         balance["nnn"] += hold.amount * hold.p
-        #         sell["nnn"].append(hold.p)
-        #         stock_hold_nnn.remove(hold)
-        #         del hold
+        for hold in stock_hold_nnn:
+            if int(day) == int(hold.enddate):
+                total["nnn"] += hold.amount * (hold.p - 1)
+                balance["nnn"] += hold.amount * hold.p
+                sell["nnn"].append(hold.p)
+                stock_hold_nnn.remove(hold)
+                del hold
    
     return sell, total
 
@@ -207,36 +208,46 @@ def cal_7(data, date):
 
 
 if __name__ == "__main__":
-    data_path = r'C:\Users\wuziyang\Documents\PyWork\trading_simulation\data\stockdata\stock_latest.csv'
+    print("start...")
+
+    data_path = r'D:\wuziyang\workfile\stock_latest.csv'
     data = pd.read_csv(data_path, sep=',', low_memory=False)
     data = data[['Symbol', 'TradingDate', 'ChangeRatio']]
     
     testYear = 2
     stockhold = 5
-    # 获取最近两年数据
-    tradedata = data[data['TradingDate'] >= today - testYear * 10000]
-    # 获取所有交易日
-    df_group = tradedata.groupby(by="TradingDate")
-    tradingdate = list(df_group.groups.keys())
-    # 回测
-    # 评估公式: score = all_add(x - 1.06) / np.var(x)
-    p, t = back_test(data, testYear, stockhold, tradingdate)
-    print("total:")
-    print("5-10:" + str(t["5_10"]))
-    # print("7:" + str(t["7"]))
-    # print("nnn:" + str(t["nnn"]))
+    # 用于test nnn
+    for i in range(0, testYear):
+        tradedata = data[data['TradingDate'] >= 10000*(2020-i) + 100]
+        tradedata = tradedata[tradedata['TradingDate'] <= 10000*(2020-i) + 400]
+    # 用于test7
+    # tradedata = data[data['TradingDate'] >= today - (testYear + 2) * 10000]
+    # tradedata = tradedata[tradedata['TradingDate'] <= today - testYear * 10000]
+    #
+    # # 获取最近n年数据
+    # tradedata = data[data['TradingDate'] >= today - testYear * 10000]
+        # 获取所有交易日
+        df_group = tradedata.groupby(by="TradingDate")
+        tradingdate = list(df_group.groups.keys())
+        # 回测
+        # 评估公式: score = all_add(x - 1.06) / np.var(x)
+        p, t = back_test(data, testYear, stockhold, tradingdate)
+        print("total:")
+        # print("5-10:" + str(t["5_10"]))
+        # print("7:" + str(t["7"]))
+        print("nnn:" + str(t["nnn"]))
 
-    np5_10 = np.array(p["5_10"])
-    # np7 = np.array(p["7"])
-    # npnnn = np.array(p["nnn"])
-    print("mean:")
-    print(np.mean(np5_10))
-    # print(np.mean(np7))
-    # print(np.mean(npnnn))
-    score5_10 = np.mean(np5_10) / np.var(np5_10)
-    # score7 = np.mean(np7) / np.var(np7)
-    # scorennn = np.mean(npnnn) / np.var(npnnn)
-    print("score:")
-    print("5-10:" + str(score5_10))
-    # print("7:" + str(score7))
-    # print("nnn:" + str(scorennn))
+        # np5_10 = np.array(p["5_10"])
+        # np7 = np.array(p["7"])
+        npnnn = np.array(p["nnn"])
+        print("mean:")
+        # print(np.mean(np5_10))
+        # print(np.mean(np7))
+        print(np.mean(npnnn))
+        # score5_10 = np.mean(np5_10) / np.var(np5_10)
+        # score7 = np.mean(np7) / np.var(np7)
+        scorennn = np.mean(npnnn) / np.var(npnnn)
+        print("score:")
+        # print("5-10:" + str(score5_10))
+        # print("7:" + str(score7))
+        print("nnn:" + str(scorennn))
